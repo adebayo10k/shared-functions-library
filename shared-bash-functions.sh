571fb51a68254e4f9bbb06dfddde26c3f4af1aa7
damola adebayo <adebayo10k@gmail.com>
2021-07-12 04:40:56 +0100

    adds more shared function definitions

--- shared-bash-functions.sh
+++ shared-bash-functions.sh
@@ -27,3 +27,53 @@ function lib10k_exit_with_error()
 }
 
 ################################################################
+# programs must all be in the PATH for both regular and root user.
+# they're not built-ins
+# could use their absolute paths, but these may vary with host system
+function check_program_requirements() 
+{
+	declare -a dependencies=( "$@" )
+
+	echo
+	echo "PROGRAM REQUIREMENTS CHECK"
+	echo "=========================="
+
+	for program_name in "${dependencies[@]}"
+	do
+	  if type "$program_name" >/dev/null 2>&1
+		then
+			echo "$program_name already installed OK"
+		else
+			echo "${program_name} is NOT installed."
+			echo "program dependencies are: ${program_dependencies[@]}"
+  			msg="Required program not found. Exiting now..."
+			lib10k_exit_with_error "$E_REQUIRED_PROGRAM_NOT_FOUND" "$msg"
+		fi
+	done
+
+}
+
+################################################################
+
+function display_program_header()
+{
+	#program_title="${1}"
+	#original_author="${2}"
+
+	# Display a program header and give user option to leave if here in error:
+    echo
+    echo -e "			\033[33m==============================================================\033[0m";
+    echo -e "			\033[33m||			Welcome to the $program_title			\033[0m";
+	echo -e "			\033[33m||			Author: $original_author					\033[0m"; 
+	echo -e "			\033[33m==============================================================\033[0m";
+    echo
+
+	if type cowsay > /dev/null 2>&1
+	then
+		cowsay "Hello, ${USER}!"
+	fi
+}
+
+################################################################
+
+
