6f72f29d2f0c190da02936e37aee9af88c85821d
damola adebayo <adebayo10k@gmail.com>
2021-07-12 11:43:54 +0100

    adds more shared function definitions

--- shared-bash-functions.sh
+++ shared-bash-functions.sh
@@ -75,5 +75,131 @@ function display_program_header()
 }
 
 ################################################################
+# - trim leading and trailing space characters
+function sanitise_input_spaces_both_ends()
+{
+	test_line="${1}"
+	echo "test line on entering "${FUNCNAME[0]}" is: $test_line" && echo
+
+	# while....?
+
+	# TRIM TRAILING AND LEADING SPACES AND TABS
+	test_line=${test_line%%[[:blank:]]}
+	test_line=${test_line##[[:blank:]]}
+
+	echo "test line after space cleanups in "${FUNCNAME[0]}" is: $test_line" && echo
+}
+################################################################
+# for example to prepare 
+function remove_trailing_fwd_slash()
+{
+	test_line="${1}"
+	echo "test line on entering "${FUNCNAME[0]}" is: $test_line" && echo
+
+	# TRIM TRAILING / FOR ABSOLUTE PATHS:
+    while [[ "$test_line" == *'/' ]]
+    do
+        echo "FOUND ENDING SLASH"
+        test_line=${test_line%'/'}
+    done    
+
+	echo "test line after trim cleanups in "${FUNCNAME[0]}" is: $test_line" && echo
+}
+################################################################
+# for example, to prepare file path to append another
+function remove_leading_fwd_slash()
+{
+	test_line="${1}"
+	echo "test line on entering "${FUNCNAME[0]}" is: $test_line" && echo
+
+	# TRIM LEADING / FOR RELATIVE PATHS:
+    while [[ "$test_line" == '/'* ]]
+    do
+        echo "FOUND LEADING SLASH"
+        test_line=${test_line#'/'}
+    done  
+
+	echo "test line after trim cleanups in "${FUNCNAME[0]}" is: $test_line" && echo
+}
+################################################################
+# 
+function make_abs_pathname()
+{
+	test_line="${1}"
+	echo "test line on entering "${FUNCNAME[0]}" is: $test_line" && echo
+
+	while [[ "$test_line" == *'/' ]] ||\
+	 [[ "$test_line" == *[[:blank:]] ]] ||\
+	 [[ "$test_line" == [[:blank:]]* ]]
+	do
+		# call the appropriate set of functions
+		sanitise_input_spaces_both_ends "$test_line"
+		remove_trailing_fwd_slash "$test_line"
+	done
+
+	echo "test line after trim cleanups in "${FUNCNAME[0]}" is: $test_line" && echo
+}
+################################################################
+function make_rel_pathname()
+{
+	test_line="${1}"
+	echo "test line on entering "${FUNCNAME[0]}" is: $test_line" && echo
+
+	while [[ "$test_line" == *'/' ]] ||\
+	 [[ "$test_line" == *[[:blank:]] ]] ||\
+	 [[ "$test_line" == [[:blank:]]* ]] ||\
+	 [[ "$test_line" == '/'* ]]
+	do
+		# call the appropriate set of functions
+		sanitise_input_spaces_both_ends "$test_line"
+		remove_trailing_fwd_slash "$test_line"
+		remove_leading_fwd_slash "$test_line"
+	done
+
+	echo "test line after trim cleanups in "${FUNCNAME[0]}" is: $test_line" && echo
+}
+################################################################
+
+# generic need to test for access to a directory. 3 logical states:
+# 0. directory exists and is cd-able
+# 1. directory exists and is not cd-able
+# 2. neither of those, so directory does NOT exist
+# 
+function test_dir_path_access
+{
+	echo && echo "ENTERED INTO FUNCTION ${FUNCNAME[0]}" && echo
+
+	test_result=
+	test_dir_fullpath=$1
+
+	echo "test_dir_fullpath is set to: $test_dir_fullpath"
+
+	if [ -d "$test_dir_fullpath" ] && cd "$test_dir_fullpath" 2>/dev/null
+	then
+		# directory file found and accessible
+		echo "directory "$test_dir_fullpath" found and accessed ok" && echo
+		test_result=0
+	elif [ -d "$test_dir_fullpath" ] ## 
+	then
+		# directory file found BUT NOT accessible CAN'T RECOVER FROM THIS
+		echo "directory "$test_dir_fullpath" found, BUT NOT accessed ok" && echo
+		test_result=1
+		echo "Returning from function \"${FUNCNAME[0]}\" with test result code: $E_FILE_NOT_ACCESSIBLE"
+		return $E_FILE_NOT_ACCESSIBLE
+	else
+		# -> directory not found: THIS CAN BE RESOLVED BY CREATING THE DIRECTORY
+		test_result=1
+		echo "Returning from function \"${FUNCNAME[0]}\" with test result code: $E_REQUIRED_FILE_NOT_FOUND"
+		return $E_REQUIRED_FILE_NOT_FOUND
+	fi
+
+	echo && echo "LEAVING FROM FUNCTION ${FUNCNAME[0]}" && echo
+
+	return "$test_result"
+}
+################################################################
+
+
+
 
 
